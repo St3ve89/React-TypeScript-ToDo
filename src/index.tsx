bc27c78bd5f0f937aa8b5cc932af58b0c28ab738
Istvan Acs <istvan.acs89@gmail.com>
2019-01-05 11:19:51 +0000

    Testing scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import './index.scss'
 import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
